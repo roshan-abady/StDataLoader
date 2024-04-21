f006754b2058aefdb985260a4c153521c1c66462
Roshan Abady <roshan.abady@myob.com>
2024-04-21 11:18:00 +1000

    Add mapping.csv file and remove get_snow_ddl.py script

--- snowloader_app.py
+++ snowloader_app.py
@@ -9,6 +9,10 @@ from snowflake.snowpark import Session
 
 warnings.filterwarnings('ignore', category=FutureWarning, module='pyarrow.pandas_compat')
 
+def load_mapping():
+    # Load the mapping CSV from the 'mapping' folder
+    return pd.read_csv('./mapping/mapping.csv')
+
 def check_session_status(session):
     try:
         result = session.sql("SELECT CURRENT_TIMESTAMP").collect()
@@ -24,23 +28,23 @@ def format_table_name(name):
 
 def create_stage(session, stage_name):
     session.sql(f"CREATE OR REPLACE STAGE {stage_name}").collect()
-    st.info(f"Stage {stage_name} created or replaced.")
+    st.info(f":building_construction: Stage is created.")
 
 def upload_data_to_stage(session, stage_name, file_path):
     session.file.put(file_path, stage_name)
-    st.success(f"Data uploaded to stage {stage_name}.")
+    st.info(f":clapper: Data is Staged.")
 
 def truncate_table(session, table_name):
     try:
         session.sql(f"TRUNCATE TABLE IF EXISTS {table_name}").collect()
-        st.success(f"Table {table_name} successfully truncated.")
+        st.info(f":haircut: Table Truncated.")
     except Exception as e:
         st.error(f"Failed to truncate table {table_name}: {e}")
 
 def bulk_copy_into(session, stage_name, table_name, file_type):
     try:
         session.sql(f"COPY INTO {table_name} FROM @{stage_name} FILE_FORMAT = (TYPE='{file_type}', SKIP_HEADER=1, FIELD_DELIMITER=',', TRIM_SPACE=FALSE, FIELD_OPTIONALLY_ENCLOSED_BY='\"', REPLACE_INVALID_CHARACTERS=TRUE, DATE_FORMAT='YYYY-MM-DD', TIME_FORMAT='HH24:MI:SS.FF', TIMESTAMP_FORMAT='YYYY-MM-DD HH24:MI:SS TZH:TZM') ON_ERROR=CONTINUE").collect()
-        st.success("Data successfully copied.")
+        st.success(":white_check_mark: Data is Loaded.")
     except Exception as e:
         st.error(f"Failed to copy data: {e}")
 
@@ -54,12 +58,12 @@ def snowflake_upload_with_stage(session, df, stage_name, table_name, file_type):
     os.remove(temp_file)
 
 def modify_snowflake_connection_parameters(default_config):
-    st.write("Snowflake Connection Configuration:")
+    st.write("Connection Config:")
     schema_options = ['TRANSFORMED_PROD', 'RAW']
     available_roles = ['OPERATIONS_ANALYTICS_OWNER_AD', 'OPERATIONS_ANALYTICS_OWNER']
-    
-    selected_schema = st.radio("Choose Schema", options=schema_options, index=schema_options.index(default_config['schema']))
-    selected_role = st.radio("Choose Role", options=available_roles, index=available_roles.index(default_config['role']))
+    col1, col2 = st.columns(2)
+    selected_schema = col1.radio("Choose Schema", options=schema_options, index=schema_options.index(default_config['schema']))
+    selected_role = col2.radio("Choose Role", options=available_roles, index=available_roles.index(default_config['role']))
     
     if selected_schema != default_config['schema'] or selected_role != default_config['role']:
         default_config['schema'] = selected_schema
@@ -85,12 +89,12 @@ if 'snowflake_session' not in st.session_state:
 
 session = st.session_state.snowflake_session
 if check_session_status(session) != "Active":
-    st.error("Failed to initialize or session is inactive.")
-else:
-    st.success("Snowflake session is active and ready.")
+    st.error("Failed to initialise or session is inactive.")
+
+mapping_df = load_mapping()  # Load the mapping data
 
 # Streamlit UI layout
-st.title("Data Management")
+st.title(":snowflake: Snow Loader")
 modify_snowflake_connection_parameters(default_config)  # Display configuration options
 
 uploaded_file = st.file_uploader("Choose a CSV or Excel file", type=["csv", "xls", "xlsx"], key="file_uploader")
@@ -100,11 +104,22 @@ if uploaded_file:
     table_name = format_table_name(uploaded_file.name.split(".")[0])
     df = pd.read_csv(uploaded_file) if file_type == 'csv' else pd.read_excel(uploaded_file)
     st.write("Preview of Data:")
-    st.dataframe(df.head())
+    st.dataframe(df,height=200, use_container_width=True,) 
 
+    # Check if there is a mapping entry for the uploaded file
+    mapping = mapping_df[mapping_df['TABLE_NAME'].str.upper() == table_name.upper()]
+    if not mapping.empty:
+        # Update config if there are changes
+        new_schema = mapping['TABLE_SCHEMA'].values[0]
+        new_role = mapping['TABLE_OWNER'].values[0]
+        if new_schema != default_config['schema'] or new_role != default_config['role']:
+            default_config['schema'] = new_schema
+            default_config['role'] = new_role
+            session.reconfigure(default_config)  # Apply new configuration
+            
     if st.button("Upload to Snowflake", key="upload_button"):
         with st.spinner("Uploading to Snowflake..."):
-            stage_name = f"{table_name}_stage"
+            stage_name = f"{table_name}_STAGE"
             if check_session_status(session) == "Active":
                 snowflake_upload_with_stage(session, df, stage_name, table_name, file_type)
             else:
